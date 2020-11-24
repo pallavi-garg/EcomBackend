db7093e13fb7fe1847dbee989fadb62bdb929e4f
prgupta <prgupta@lutron.com>
2020-11-24 17:22:57 +0530

    serializaton attribute added

--- Shared/ProductService.Shared/Model/FeatureDetail.cs
+++ Shared/ProductService.Shared/Model/FeatureDetail.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace ProductService.Shared.Model
 {
-
+    [DataContract]
     public class FeatureDetail
     {
+        [DataMember]
         public string Key { get; set; }
+        
+        [DataMember]
         public string Value { get; set; }
     }
 }
--- Shared/ProductService.Shared/Model/ImageDetail.cs
+++ Shared/ProductService.Shared/Model/ImageDetail.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace ProductService.Shared.Model
 {
-
+    [DataContract]
     public class ImageDetail
     {
+        [DataMember]
         public string ThumbnailUrl { get; set; }
+        
+        [DataMember]
         public string BaseUrl { get; set; }
     }
 }
